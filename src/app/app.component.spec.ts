a794efdbf809bf1f3d32bf9ff011d45d6b6f1b46
Ivan Zdravkovic <izdravkovic@localhost.localdomain>
2018-10-08 14:08:34 +0200

    maa-009

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('my-angular-app');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render iz-nav in a div tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-angular-app!');
+    expect(compiled.querySelector('div > iz-nav')).toBeDefined('Welcome to my-angular-app!');
   });
 });
